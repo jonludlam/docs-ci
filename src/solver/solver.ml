ea19958d5382318f3abf7e7489df0cd9a3e8f7eb
Jon Ludlam <jon@recoil.org>
2024-09-26 00:25:02 +0100

    More fixes

--- src/solver/solver.ml
+++ src/solver/solver.ml
@@ -32,7 +32,7 @@ let universes ~packages (resolutions : OpamPackage.t list) =
           |> OpamFile.OPAM.depends
           |> OpamFilter.partial_filter_formula
                (OpamFilter.deps_var_env ~build:true ~post:false ~test:false
-                  ~doc:false ~dev:false)
+                  ~doc:false ~dev_setup:false ~dev:false)
           |> get_names
           |> OpamPackage.Name.Set.of_list
         in
